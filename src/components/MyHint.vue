0886a7c151bd7a1661409438feff894df06ae806
CTian <2679125259@qq.com>
2023-07-25 23:58:57 +0800

    添加默认页面

---
+++ src/components/MyHint.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <pre class="kuma">
+　　　./ ハヽ
+　 　 ;|　0:0|
+　　　:|ヽω/
+.　　　:|| '^ |.
+　　　 :h ノ
+　　 　 :|/:
+　　　 / |､　　 　＼
+　　　;|　､､_､　　　 ＼
+　　　;|ﾐ| ￣＾＼ 　 　 ＼
+
+　　　　　　　　　　　 　　　　＿_　　 ＿_
+　　　　　　　　　　　　　 　＜　 ヽ／ ∠_
+　　　 　 　 　 　 　 　　　,-ｙ-ｒ─ｙ-ｒ-,、_≧ _
+　　　　　　　　　 　　　,-ゝ_,'--'─'--＾、ｲ,ヽ,　＞
+　　　　　　,r'"´「jヽ .〈, -´　 　 　　　　 ヽノ、', }ﾊ      没有搜索关键词啊~!
+　 　 　 　/　　 | |　ﾞ, .,'　,ｲ　,　,　、　u.、 Yｲ　',　}|　　
+　　 　 　 l l |　|| |　 !ｲ/　|:/|:∧　:|＼{＼ ! }　i|　i|!　　
+　 　　 　 ! 」」_」 |」/ 八　 { -- ＼l --- ﾚi厶ノ　八　
+　　　　　　　 　| |　　 .ｲ〈人　 r‐┐.u., ｲ　ﾉ !　/
+　　　　　　　　 /「＼　　　y.｀＞ryr＜ 〈yｲ,ｲノ´　　　
+　　　　　　　　 L ソ/ﾄ､　/.:.:/　ujuj　i!/.::::::.:＼　　　
+　　　　 　 　 　 ＼.::::ゞﾘﾚ7/　　V/　ハ.:::::::.::／
+　　　　　　　 　 　 ＼:ゞﾘ::ﾘ　　　　　j /ﾊ.::;ｲ
+　　　　　　　　　　　　＼:ノ｀¨`ｰ─‐{ｲ　 !:.:::＼
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.kuma {
+  font-size: 1em;
+  line-height: 1.1 !important;
+  font-family: "ＭＳ Ｐゴシック", "MS PGothic", Monapo, "IPA モナー Pゴシック",
+    IPAMonaPGothic, Mona, RobotoJAA, "梅Pゴシック", Textar, sans-serif;
+  margin: 1em;
+  padding: 0.5em;
+  _padding-bottom: 1em;
+  border: dotted 2px #ffffff !important;
+  overflow: auto;
+}
+</style>
